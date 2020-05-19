437c6e6e1569b3d5b66f99f89e634fda03eb1497
hien <hien07072001@gmail.com>
2020-05-19 11:27:12 +0700

    test Controller

--- src/test/java/CustomerControllerTest.java
+++ src/test/java/CustomerControllerTest.java
@@ -1,8 +1,16 @@
 import com.codegym.cms.controller.CustomerController;
+import com.codegym.cms.service.Customer.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.PagedResourcesAssemblerArgumentResolver;
+import org.springframework.test.context.junit.jupiter.SpringJUnitJupiterConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -11,18 +19,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringJUnitJupiterConfig(CustomerControllerTestConfig.class)
 class CustomerControllerTest {
-    @Test
-    void  assertionWorked(){
-
-        int  actual=1+1;
-
-        int excepted= 2;
-
-        assertEquals( actual,excepted);
-
-
-    }
+//    @Test
+//    void  assertionWorked(){
+//
+//        int  actual=1+1;
+//
+//        int excepted= 2;
+//
+//        assertEquals( actual,excepted);
+//
+//
+//    }
+    @Autowired
+    private CustomerService customerService;
 
     private MockMvc mockMvc;
 
@@ -32,12 +45,16 @@ class CustomerControllerTest {
     @BeforeEach
     void setup(){
         MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(customerController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .build();
     }
+
     @Test
     void customersListPageIsExits() throws  Exception{
         mockMvc
-                .perform(get("/customer/list"))
+                .perform(get("/customer"))
                 .andExpect(status().is(200));
     }
 
--- src/test/java/CustomerControllerTestConfig.java
+++ src/test/java/CustomerControllerTestConfig.java
@@ -1,2 +1,29 @@
+import com.codegym.cms.service.Customer.CustomerService;
+import com.codegym.cms.service.Customer.CustomerServiceImpl;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.codegym.cms")
 public class CustomerControllerTestConfig {
+    @Bean
+    public CustomerService customerService(){
+        return Mockito.mock(CustomerServiceImpl.class);
+    }
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("cms")
+                .build();
+    }
+
+
 }
